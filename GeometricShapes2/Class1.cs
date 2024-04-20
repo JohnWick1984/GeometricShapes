d2256abda7b97d8a38a729aad9b83224e9f1d43c
Евгений Шумаков <daredevil1984@list.ru>
2024-04-20 10:17:52 +0500

    Добавьте файлы проекта.

---
+++ GeometricShapes2/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GeometricShapesLibrary
+{
+    public class GeometricShapes
+    {
+        public static double SquareArea(double side)
+        {
+            return side * side;
+        }
+
+        public static double RectangleArea(double length, double width)
+        {
+            return length * width;
+        }
+
+        public static double TriangleArea(double @base, double height)
+        {
+            return 0.5 * @base * height;
+        }
+    }
+}
---
+++ TextUtils/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TextUtilsLibrary
+{
+    public static class TextUtils
+    {
+        public static bool IsPalindrome(this string str)
+        {
+            str = str.ToLower();
+            int left = 0;
+            int right = str.Length - 1;
+
+            while (left < right)
+            {
+                if (!char.IsLetterOrDigit(str[left]))
+                {
+                    left++;
+                    continue;
+                }
+                if (!char.IsLetterOrDigit(str[right]))
+                {
+                    right--;
+                    continue;
+                }
+
+                if (str[left] != str[right])
+                    return false;
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+
+        public static int WordCount(this string str)
+        {
+            return str.Split(new char[] { ' ', '.', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public static int SentenceCount(this string str)
+        {
+            return str.Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public static int CharacterCount(this string str)
+        {
+            return str.Length;
+        }
+    }
+}
